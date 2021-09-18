382721403c0ac56ca34ffc58138195431983a4a5
sidmfkid <newskinlifestore@gmail.com>
2021-09-18 01:56:24 -0400

    added skills. work, need to make responsive

--- js/type.js
+++ js/type.js
@@ -31,28 +31,34 @@ typewriter
 
   $('#welcome-btn').on( 'click', function () {
       $('.welcome').toggleClass('open').toggleClass('closed')
-      $('.aboutme').toggleClass('open').toggleClass('closed')
-    setTimeout(() => {
-        typewriter.stop()
-    },2000)
+      $('.aboutme').toggleClass('open').toggleClass('closed').toggleClass('next')
+
+
+
+      setTimeout(() => {
+        $('.aboutme__content--card').toggleClass('open').toggleClass('closed');
+    },500)
+
 
     setTimeout(() => {
-        $('.aboutme__content--card').toggleClass('open').toggleClass('closed');
-    },700)
+        typewriter.stop()
+        $('.welcome').toggleClass('next')
+    },500)
+
 
     setTimeout(() => {
         $('#goBack').toggleClass('open').toggleClass('closed')
         $('#keepGoing').toggleClass('open').toggleClass('closed')
-    },2300)
+    },600)
 
     setTimeout(() => {
         $('.aboutme__content--card').toggleClass('animation');
-    },2500)
+    },2300)
   })
 
   $('#goBack').on('click', function () {
-    $('.welcome').toggleClass('open').toggleClass('closed')
-    $('.aboutme').toggleClass('open').toggleClass('closed')
+    $('.welcome').toggleClass('open').toggleClass('closed').toggleClass('next')
+    $('.aboutme').toggleClass('open').toggleClass('closed').toggleClass('next')
     $('.aboutme__content--card').toggleClass('closed').toggleClass('open');
     typewriter.start()
     $('.aboutme__content--card').toggleClass('animation');
@@ -67,50 +73,103 @@ typewriter
 //  setTimeout(() => {
 //      $('.aboutme__content--card').toggleClass('animation');
 //  }, 100)
+// setTimeout(() => {
+//     $('.aboutme__content--card').toggleClass('move-left');
+// }, 300)
+
+setTimeout(()=> {
+  $('.aboutme__content--info').toggleClass('next')
+}, 800)
+
+setTimeout(() => {
+    $('.aboutme__content').toggleClass('show-info')
+    $('.aboutme__content--info').toggleClass('show-info')
+    // $('.aboutme__content--card').toggleClass('move-left');
+
+},800);
 
  setTimeout(() => {
     $('.aboutme__content--card').toggleClass('animation');
 }, 2000)
 
-
-setTimeout(() => {
-    $('.aboutme__content--card').toggleClass('move-left');
-}, 300)
-
-
-
-
-
-setTimeout(() => {
-    $('.aboutme__content').toggleClass('show-info')
-    $('.aboutme__content--info').toggleClass('show-info')
-    $('.aboutme__content--card').toggleClass('move-left');
-
-},800);
-    $(this).toggleClass('fas fa-chevron-circle-right').toggleClass('fas fa-chevron-circle-down')
-
-    // var $info = $('.aboutme__content--info')
-
-    // setTimeout(() => {
-    //     if ($info.hasClass('show-info') === false)
-    //     {
-    //         console.log($info.hasClass('show-info'));
-    //         $('.aboutme__content--info').toggleClass('set')
-    //     }
-    //     setTimeout(() => {
-    //         if ($info.hasClass('show-info') === false)
-    //     {
-    //         console.log($info.hasClass('show-info'));
-    //         $('.aboutme__content--info').toggleClass('set')
-    //     }
-    //     },700)
-    // }, 700)
-
-
-
-
-
+  $(this).toggleClass('fas fa-chevron-circle-right').toggleClass('fas fa-chevron-circle-down')
+    $('#nextSection').toggleClass('open');
   })
+
+  
+  
+  $('#nextSection').on('click', function () {
+
+    setTimeout(() => {
+    $('.aboutme').toggleClass('blur').delay(900).toggleClass('next')
+
+    }, 1000)
+
+    setTimeout(() => {
+      $('.skills').toggleClass('next').toggleClass('closed').toggleClass('open')
+      $('.skills__content').toggleClass('closed').delay(900).toggleClass('open')
+      $('#backToAbout').toggleClass('next')
+      $('#toHistory').toggleClass('next')
+    }, 1000)
+
+    setTimeout(()=> {
+      $('.bar > span').css('animation', 'grow 1s ease-in-out forwards')
+    },2000)
+
+    $('.aboutme__content').toggleClass('blur').delay(900).toggleClass('next')
+   })
+
+   $('#backToAbout').on('click', function () {
+
+      $('.skills').toggleClass('open').toggleClass('next')
+      $('.skills__content').toggleClass('closed')
+      $('#toHistory').toggleClass('blur').delay(900).toggleClass('next').toggleClass('blur')
+      $(this).toggleClass('blur').delay(900).toggleClass('next').toggleClass('blur')
+
+    $('.aboutme__content').toggleClass('next').toggleClass('blur')
+    $('.aboutme').toggleClass('next').toggleClass('blur')
+
+    })
+
+    $('#toHistory').on('click', function () {
+      $('.skills__content').toggleClass('closed')
+
+
+      $('#backToAbout').toggleClass('blur').delay(900).toggleClass('next').toggleClass('blur')
+      $(this).toggleClass('blur').delay(900).toggleClass('next').toggleClass('blur')
+
+      setTimeout(()=> {
+        $('.skills__content').toggleClass('next')
+        $('.skills').toggleClass('next')
+      },1000)
+
+      setTimeout(()=> {
+        $('.work').toggleClass('next')
+      },1100)
+
+
+     })
+
+     $('#backToSkills').on('click', function () { 
+
+      $('.skills__content').toggleClass('next')
+      $('.skills').toggleClass('open').delay(900).toggleClass('next').toggleClass('open')
+      $('.work').toggleClass('next')
+      $('#backToAbout').toggleClass('next').toggleClass('unblur')
+      $('#toHistory').toggleClass('next').toggleClass('unblur')
+
+      setTimeout(()=> {
+        $('.skills__content').toggleClass('closed')
+        // $('.skills__content').toggleClass('closed')
+
+      },1500)
+
+      setTimeout(()=> {
+        $('.bar > span').css('animation', 'grow 1s ease-in-out forwards')
+      },1100)
+
+      })
+
 });
 
 
