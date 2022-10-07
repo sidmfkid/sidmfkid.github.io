e507e96db795b906dc031545e7f6b416f6092675
sidmfkid <newskinlifestore@gmail.com>
2022-10-07 01:15:05 -0400

    first commit 3.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,212 @@
-import logo from './logo.svg';
-import './App.css';
+import bgImage from "./BG-noblur.jpg";
+import "./App.css";
+import {
+  ScrollContainer,
+  ScrollPage,
+  Animator,
+  Sticky,
+  Fade,
+  FadeIn,
+  MoveIn,
+  MoveOut,
+  batch,
+  ZoomIn,
+  Zoom,
+  StickyIn,
+} from "react-scroll-motion";
+import Card from "./components/Card";
+import projects from "./projects.json";
+
+const ZoomInScrollOut = batch(FadeIn(), MoveIn(500, 0), MoveOut(-500, 0));
 
 function App() {
+  const cards = projects.map((v) => {
+    return (
+      <Card
+        key={v.title}
+        title={v.title}
+        year={v.year}
+        type={v.type}
+        langs={v?.languages}
+        tools={v?.tools}
+        stack={v?.stack}
+      />
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ScrollContainer snap="proximity">
+      {/* <img className="bg-image" src={bgImage} alt=""></img> */}
+      {/* <div className="bg"> */}
+      <ScrollPage page={0}>
+        <Animator animation={batch(Fade(), MoveOut(0, -500))}>
+          <div className="main">
+            <section className="section">
+              <div className="section__container">
+                <div className="section__title">Sidney Moore</div>
+                <Animator animation={batch(MoveOut(-500, 0), Fade())}>
+                  <div className="section__divider"></div>
+                </Animator>
+                <Animator animation={batch(MoveOut(500, 0), Fade())}>
+                  <div className="section__subTitle">
+                    Full Stack Web Developer
+                  </div>
+                </Animator>
+              </div>
+            </section>
+
+            {/* <section className="section">
+        <div className="section__container">
+          <div className="section__title">Sidney Moore</div>
+          <div className="section__divider"></div>
+          <div className="section__subTitle">Full Stack Web Developer</div>
+        </div>
+      </section>
+      <section className="section">
+        <div className="section__container">
+          <div className="section__title">Sidney Moore</div>
+          <div className="section__divider"></div>
+          <div className="section__subTitle">Full Stack Web Developer</div>
+        </div>
+      </section> */}
+          </div>
+        </Animator>
+      </ScrollPage>
+      <ScrollPage page={1}>
+        <div className="main">
+          <section className="section">
+            <div className="section__container">
+              <Animator animation={FadeIn()}>
+                <div className="section__title">About Me</div>
+              </Animator>
+              <Animator
+                animation={batch(MoveIn(-500, 0), Fade(), MoveOut(800, 0))}
+              >
+                <div className="section__divider"></div>
+              </Animator>
+              <div className="section__subTitle">
+                <Animator animation={ZoomInScrollOut}>
+                  <span>Location: Indianapolis, Indiana</span>
+                  <span>
+                    Hobbies: Music Production, Go Karting, Rocket League
+                  </span>
+                  <span>
+                    Favorite Software: FL Studio, Pro Tools, Adobe Suite
+                  </span>
+                </Animator>
+              </div>
+            </div>
+          </section>
+
+          {/* <section className="section">
+        <div className="section__container">
+          <div className="section__title">Sidney Moore</div>
+          <div className="section__divider"></div>
+          <div className="section__subTitle">Full Stack Web Developer</div>
+        </div>
+      </section>
+      <section className="section">
+        <div className="section__container">
+          <div className="section__title">Sidney Moore</div>
+          <div className="section__divider"></div>
+          <div className="section__subTitle">Full Stack Web Developer</div>
+        </div>
+      </section> */}
+        </div>
+      </ScrollPage>
+
+      <ScrollPage page={2}>
+        <div className="main">
+          <section className="section">
+            <div className="section__container">
+              <Animator animation={FadeIn()}>
+                <div className="section__title">Projects</div>
+              </Animator>
+              <Animator
+                animation={batch(MoveIn(-500, 0), Fade(), MoveOut(800, 0))}
+              >
+                <div className="section__divider"></div>
+              </Animator>
+              <div className="section__body">
+                <div className="cards">{cards}</div>
+              </div>
+              <div className="section__footer hide">
+                <button className="back">
+                  <span className="material-symbols-outlined">arrow_back</span>
+                </button>
+                <button className="next">
+                  <span className="material-symbols-outlined">
+                    arrow_forward
+                  </span>
+                </button>
+              </div>
+            </div>
+          </section>
+
+          {/* <section className="section">
+        <div className="section__container">
+          <div className="section__title">Sidney Moore</div>
+          <div className="section__divider"></div>
+          <div className="section__subTitle">Full Stack Web Developer</div>
+        </div>
+      </section>
+      <section className="section">
+        <div className="section__container">
+          <div className="section__title">Sidney Moore</div>
+          <div className="section__divider"></div>
+          <div className="section__subTitle">Full Stack Web Developer</div>
+        </div>
+      </section> */}
+        </div>
+      </ScrollPage>
+      <ScrollPage page={3}>
+        <div className="main skills">
+          <section className="section">
+            <div className="section__container">
+              <Animator animation={FadeIn()}>
+                <div className="section__title">Skills/Experience</div>
+              </Animator>
+              <Animator
+                animation={batch(MoveIn(-500, 0), Fade(), MoveOut(800, 0))}
+              >
+                <div className="section__divider"></div>
+              </Animator>
+              <div className="section__body skills">
+                <Card title="Languages/Frameworks/Libraries" section={"libs"} />
+
+                <Card title="Tools" section={"tools"} />
+              </div>
+              {/* <div className="section__footer">
+                <button className="back">
+                  <span className="material-symbols-outlined">arrow_back</span>
+                </button>
+                <button className="next">
+                  <span className="material-symbols-outlined">
+                    arrow_forward
+                  </span>
+                </button>
+              </div> */}
+            </div>
+          </section>
+
+          {/* <section className="section">
+        <div className="section__container">
+          <div className="section__title">Sidney Moore</div>
+          <div className="section__divider"></div>
+          <div className="section__subTitle">Full Stack Web Developer</div>
+        </div>
+      </section>
+      <section className="section">
+        <div className="section__container">
+          <div className="section__title">Sidney Moore</div>
+          <div className="section__divider"></div>
+          <div className="section__subTitle">Full Stack Web Developer</div>
+        </div>
+      </section> */}
+        </div>
+      </ScrollPage>
+      {/* </div> */}
+    </ScrollContainer>
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,229 @@
+import {
+  Animator,
+  Sticky,
+  Fade,
+  FadeIn,
+  MoveIn,
+  MoveOut,
+  batch,
+  ZoomIn,
+  Zoom,
+  StickyIn,
+} from "react-scroll-motion";
+
+const reqLibs = require.context("../svgs/libs", true, /\.svg$/);
+const svgsLibs = reqLibs.keys().map((key) => reqLibs(key));
+
+const reqTools = require.context("../svgs/tools", true, /\.svg$/);
+const svgsTools = reqTools.keys().map((key) => reqTools(key));
+
+const libInfo = [
+  {
+    name: "Liquid",
+    ex: "2yrs",
+  },
+  {
+    name: "CSS",
+    ex: "3yrs",
+  },
+  {
+    name: "EJS",
+    ex: "2yrs",
+  },
+  {
+    name: "HTML",
+    ex: "3yrs",
+  },
+  {
+    name: "Javascript",
+    ex: "3yrs",
+  },
+  {
+    name: "MongoDB",
+    ex: "3yrs",
+  },
+  {
+    name: "Node.js",
+    ex: "3yrs",
+  },
+  {
+    name: "React.js",
+    ex: "3yrs",
+  },
+  {
+    name: "Sass/Scss",
+    ex: "3yrs",
+  },
+];
+
+const toolInfo = [
+  {
+    name: "Illustrator",
+    ex: "5yrs",
+  },
+  {
+    name: "Photoshop",
+    ex: "7yrs",
+  },
+  {
+    name: "Figma",
+    ex: "2yrs",
+  },
+  {
+    name: "Trello",
+    ex: "1yr",
+  },
+  {
+    name: "NPM",
+    ex: "2yrs",
+  },
+  {
+    name: "Github",
+    ex: "2yrs",
+  },
+  {
+    name: "Heroku",
+    ex: "2yrs",
+  },
+  {
+    name: "VS Code",
+    ex: "3yrs",
+  },
+];
+
+function Card({ title, year, type, langs, tools, stack, info, section }) {
+  const languages = langs?.map((l) => {
+    return <li key={l}>{l}</li>;
+  });
+  const cardListLang = langs && (
+    <div className="card__list">
+      <span>Languages:</span>
+      <ul>{languages}</ul>
+    </div>
+  );
+
+  const toolsLi = tools?.map((l) => {
+    return <li key={l}>{l}</li>;
+  });
+  const cardListTools = tools && (
+    <div className="card__list">
+      <span>Tools:</span>
+      <ul>{toolsLi}</ul>
+    </div>
+  );
+
+  const stackLi = stack?.map((l) => {
+    return <li key={l}>{l}</li>;
+  });
+  const cardListStack = stack && (
+    <div className="card__list">
+      <span>Stack:</span>
+      <ul>{stackLi}</ul>
+    </div>
+  );
+
+  const libs = svgsLibs.map((svg, i) => {
+    return {
+      svg: svg,
+      name: libInfo[i].name,
+      ex: libInfo[i].ex,
+    };
+  });
+
+  const toolz = svgsTools.map((svg, i) => {
+    return {
+      svg: svg,
+      name: toolInfo[i].name,
+      ex: toolInfo[i].ex,
+    };
+  });
+
+  const iconsLibs = libs.map((lib, i) => {
+    return (
+      <div className="item">
+        <Animator
+          animation={batch(FadeIn(), MoveIn(i * 100 + 50), -i * 100 + 50)}
+        >
+          <div className="item__icon">
+            <img src={lib.svg} alt=""></img>
+          </div>
+          <div className="item__info">
+            <div className="item__name">{lib.name}</div>
+            <div className="item__ex">{lib.ex}</div>
+          </div>
+        </Animator>
+      </div>
+    );
+  });
+
+  const iconsTools = toolz.map((tool, i) => {
+    return (
+      <div className="item">
+        <Animator
+          animation={batch(FadeIn(), MoveIn(i * -100 - 50), i * -100 - 50)}
+        >
+          <div className="item__icon">
+            <img src={tool.svg} alt=""></img>
+          </div>
+          <div className="item__info">
+            <div className="item__name">{tool.name}</div>
+            <div className="item__ex">{tool.ex}</div>
+          </div>
+        </Animator>
+      </div>
+    );
+  });
+
+  if (section === "libs") {
+    return (
+      <div className="card skills">
+        <div className="card__heading skills">
+          <div className="card__wrapper skills">
+            <h3 className="card__title skills">{title}</h3>
+          </div>
+          {/* <div className="card__divider skills"></div> */}
+        </div>
+        <div className="card__body skills">{iconsLibs}</div>
+      </div>
+    );
+  }
+  if (section === "tools") {
+    return (
+      <div className="card skills">
+        <div className="card__heading skills">
+          <div className="card__wrapper skills">
+            <h3 className="card__title skills">{title}</h3>
+          </div>
+          {/* <div className="card__divider skills"></div> */}
+        </div>
+        <div className="card__body skills">{iconsTools}</div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="card">
+        <div className="card__heading">
+          <div className="card__wrapper">
+            <h3 className="card__title">{title}</h3>
+            <div className="card__year">{year}</div>
+          </div>
+          <div className="card__type">{type}</div>
+          <div className="card__divider"></div>
+        </div>
+        <div className="card__body">
+          {cardListLang}
+          {cardListTools}
+          {cardListStack}
+        </div>
+        <div className="card__footer hide">
+          <button>
+            <span>More Info</span>
+            <span class="material-symbols-outlined icon">arrow_forward</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
